f57666c158a07b1b6bca4221367b54941eb123d2
Bogdan Slovyagin <bogdan@slovyagin.com>
2025-07-17 19:28:15 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const HOMEPAGE_URL = "https://slovyagin.com";
-const LOCAL_DEV_URL = "http://localhost:4321";
+const CLOUD_NAME = "slovyagin";
 const CLOUD_URL = `https://res.cloudinary.com/${CLOUD_NAME}/image`;
-const ALLOWED_REFERERS = [
-  HOMEPAGE_URL,
-  `${HOMEPAGE_URL}/`,
-  LOCAL_DEV_URL,
-  `${LOCAL_DEV_URL}/`
-];
+
+class InvalidSizeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "InvalidSizeError";
+  }
+}
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event));
 });
 
 function parseCustomUrl(url) {
-  const pathname = url.pathname.substring(1); // Remove leading slash
+  const pathname = url.pathname.substring(1);
   const [imageName] = pathname.split(".");
   const params = new URLSearchParams(url.search);
-
   let transformations = "c_fit,q_auto";
+
   if (params.has("s")) {
     const size = params.get("s");
+    const allowedSizes = ["700", "900", "1400"];
+    
+    if (!allowedSizes.includes(size)) {
+      throw new InvalidSizeError("Invalid size parameter");
+    }
+    
     transformations += `,w_${size},h_${size}`;
   }
 
-  // Remove s from params as we've handled it
   params.delete("s");
 
   const cloudinaryPath = `upload/${transformations}/v1/photos/${imageName}`;
@@ -35,6 +40,17 @@ function parseCustomUrl(url) {
 async function serveAsset(event) {
   const url = new URL(event.request.url);
   const cache = caches.default;
+  
+  try {
+    // Attempt to parse the URL regardless of cache status
+    parseCustomUrl(url);
+  } catch (error) {
+    if (error instanceof InvalidSizeError) {
+      return new Response("Unprocessable Entity", { status: 422 });
+    }
+    throw error;
+  }
+
   let response = await cache.match(event.request);
 
   if (!response) {
@@ -53,6 +69,7 @@ async function serveAsset(event) {
     const headers = new Headers(response.headers);
     headers.set("cache-control", `public, max-age=${365 * 24 * 60 * 60}`);
     headers.set("vary", "Accept, Referer");
+
     response = new Response(response.body, { ...response, headers });
     event.waitUntil(cache.put(event.request, response.clone()));
   }
@@ -60,33 +77,20 @@ async function serveAsset(event) {
   return response;
 }
 
-function checkReferer(request) {
-  const referer = request.headers.get("Referer");
-  
-  return (
-    referer && ALLOWED_REFERERS.some(allowed => referer.startsWith(allowed))
-  );
-}
-
 async function handleRequest(event) {
   const request = event.request;
-
   if (request.method !== "GET") {
     return new Response("Method not allowed", { status: 405 });
   }
 
-  if (!checkReferer(request)) {
-    return new Response("Hotlinking not allowed", {
-      status: 403,
-      headers: { "Content-Type": "text/plain" }
-    });
+  try {
+    let response = await serveAsset(event);
+    if (response.status > 399) {
+      response = new Response(response.statusText, { status: response.status });
+    }
+    return response;
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  let response = await serveAsset(event);
-
-  if (response.status > 399) {
-    response = new Response(response.statusText, { status: response.status });
-  }
-
-  return response;
 }
